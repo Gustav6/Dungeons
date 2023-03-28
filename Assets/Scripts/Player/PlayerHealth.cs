fdf5fca8037c898ab69d8ac6125d78796e8411a5
SkieoXDD <119525838+SkieoXDD@users.noreply.github.com>
2023-03-28 11:58:31 +0200

    Health system and weapon animation

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -2,16 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerHealth : MonoBehaviour
 {
-    
-    void Start()
+    public float health = 0f;
+    [SerializeField] private float maxHealth = 100f;
+    public bool isDead;
+
+    private Controller2D controller;
+    private Animator animator;
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        health = maxHealth;
     }
 
-    void Update()
+
+    public void UpdateHealth(float mod)
     {
-        
+
+        health += mod;
+
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+        else if (health <= 0f)
+        {
+            health = 0f;
+            Debug.Log("Player Respawn");
+        }
     }
+
+    public void Update()
+    {
+        if (health <= 0f)
+        {
+            isDead = true;
+            animator.SetBool("IsDead", isDead);
+        }
+
+    }
+
+
 }
